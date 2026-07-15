678d227c62a0d3ddf07b7be69280daf41a1d9e21
sayan-acharyya <acharyyas735@gmail.com>
2026-07-15 21:06:59 +0530

    model creation done

---
+++ backend/models/Parcel.js
@@ -0,0 +1,122 @@
+import { required } from "joi";
+import mongoose from "mongoose";
+
+const checkpointSchema = new mongoose.Schema({
+    location: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    status: {
+        type: String,
+        enum: ["arrived", "in_transit", "out_for_delivery", "delivered"],
+        required: true
+    },
+    timestamps: {
+        type: Date,
+        default: Date.now
+    },
+    updatedBy: {
+        type: String,
+        required: true,
+        trim: true
+    }
+}, { _id: false });
+
+const parcelSchema = new mongoose.Schema({
+    trackingid: {
+        type: String,
+        required: true,
+        unique: true,
+        index: true
+    },
+    senderName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    senderPhone: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    senderAddress: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    receiverName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    receiverPhone: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    receiverAddress: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    shipmentType: {
+        type: String,
+        enum: ["national", "international"],
+        required: true
+    },
+    originCity: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    destinationCity: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    deliveryType: {
+        type: String,
+        enum: ["sameDay", "overnight", "standard"],
+        required: true
+    },
+    parcelCategory: {
+        type: String,
+        enum: ["document",
+            "electronics",
+            "fragile",
+            "clothing",
+            "food",
+            "medicine",
+            "cosmetics",
+            "books",
+            "small_package",
+            "large_package"],
+        required: true,
+        trim: true
+    },
+    weight: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    checkpoints: [checkpointSchema]
+}, { timestamps: { createdAt: true, updatedAt: false } })
+
+export const Parcel = mongoose.model("Parcel", parcelSchema);
+
+//1:22:24
