18906d917b001255c84c79142a32ac05f1d9e8c6
sayan-acharyya <acharyyas735@gmail.com>
2026-07-17 20:29:37 +0530

    testing done

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -35,4 +35,5 @@ export const adminOnly = (req, res, next) => {
     if (!req.user || req.user.role !== "admin") {
         return res.status(403).json({ message: "Access denied.Admins only." })
     }
+    next(); 
 }
