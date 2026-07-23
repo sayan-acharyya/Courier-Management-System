ba79c027ece776a92bbcb749f3eab43ed92e98a6
sayan-acharyya <acharyyas735@gmail.com>
2026-07-23 20:26:58 +0530

    app sidebar done

--- Dashboard/src/components/AppSidebar.jsx
+++ Dashboard/src/components/AppSidebar.jsx
@@ -7,7 +7,7 @@ import {
   BarChart3,
   UserPlus
 } from "lucide-react";
-import  NavLink  from "@/components/NavLink";
+import NavLink from "@/components/NavLink";
 import { useNavigate } from "react-router-dom";
 import {
   Sidebar,
@@ -31,5 +31,76 @@ const mainItems = [
   { title: "Parcel Tracking", url: "/tracking", icon: Search },
 ];
 export function AppSidebar() {
-  return <></>;
+
+  const { state } = useSidebar();
+  const collapsed = state === "collapsed";
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate("/login")
+  }
+
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarContent>
+        <div className={`flex items-center gap-2 px-4 py-5 ${collapsed ? "justify-center" : ""}`}>
+          <div className="flex h-8 w-8 items-center justify-center rounded-lg
+           bg-sidebar-ring text-sidebar-primary font-bold text-sm">
+            IN
+          </div>
+          {
+            !collapsed && (
+              <span className="text-lg font-bold text-sidebar-foreground tracking-tight">
+                Rydex Swift
+              </span>
+            )
+          }
+        </div>
+
+        <SidebarGroup>
+          <SidebarGroupLabel className="text-sidebar-foreground/50">
+            Menu
+          </SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {
+                mainItems.map(item => {
+                  return (
+                    <SidebarMenuItem key={item.title}>
+                      <SidebarMenuButton asChild>
+                        <NavLink
+                          className="hover:bg-sidebar-accent"
+                          to={item.url}
+                          end={item.url === "/"}
+                          activeClassName="bg-sidebar-accent text-sidebar-accent-foreground font-medium"
+                        >
+                          <item.icon className="h-4 w-4" />
+                          {!collapsed && <span>{item.title}</span>}
+                        </NavLink>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  )
+                })
+              }
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton className="text-sidebar-foreground/70 
+            hover:bg-sidebar-accent hover:text-sidebar-accent-foreground"
+              onClick={handleLogout}
+            >
+              <LogOut className="w-4 h-4" /> {!collapsed && <span>Logout</span>}
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+
+    </Sidebar>
+  )
 }
